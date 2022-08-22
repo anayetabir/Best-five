0f43b45e091098b967ea7e4ea01e6f355fed3350
anayetabir <anayetabir007@gmail.com>
2022-08-23 02:30:04 +0600

    selected

--- js/selected.js
+++ js/selected.js
@@ -1,10 +1,43 @@
 const playerArray = [];
 
-function playerSelection(element){
+function playerList(playerArray) {
+    console.log(playerArray);
+    const tableBody = document.getElementById('player-name');
+    tableBody.innerHTML = "";
 
-    // console.log(element.parentNode.children[1].innerText);
+    for (let i = 0; i < playerArray.length; i++) {
+
+        if (i > 4) {
+            alert("Limit Over");
+            break;
+        }
+
+        const name = playerArray[i].playerName;
+        const tr = document.createElement('tr');
+        tr.innerHTML =
+            `
+        <th>${i + 1}</th>
+        <td>${name}</td>
+        
+        `;
+        tableBody.appendChild(tr);
+    }
+
+}
+
+
+
+function playerSelection(element) {
 
     const playerName = element.parentNode.children[1].innerText;
-    playerArray.push(playerName);
-    console.log(playerArray);
+
+    const playerObj = {
+        playerName: playerName,
+    }
+
+    playerArray.push(playerObj);
+
+    document.getElementById("total-added-player").innerText = playerArray.length;
+
+    playerList(playerArray);
 }
